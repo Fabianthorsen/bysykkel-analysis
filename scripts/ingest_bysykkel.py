a0a180b0b1d1afa17936ffead32a16d371bb8de9
Fabian Thorsen <thorsen.fabian@gmail.com>
2023-01-20 14:23:04 +0100

    adding README and ignore

--- scripts/ingest_bysykkel.py
+++ scripts/ingest_bysykkel.py
@@ -7,14 +7,13 @@ BASE_URL = "https://gbfs.urbansharing.com"
 LOCALE = "oslobysykkel.no"
 FILE_NAMES = ["station_information", "station_status"]
 OUTDIR = "./data"
-
-timestamp = str(datetime.now().date()).replace(" ", "_")
+TIMESTAMP = str(datetime.now().date()).replace(" ", "_")
 
 if not os.path.exists(OUTDIR):
     os.mkdir(OUTDIR)
 
 for file in FILE_NAMES:
     r = requests.get(f"{BASE_URL}/{LOCALE}/{file}.json")
-    with open(f"{OUTDIR}/{file}_{timestamp}.json", "w") as f:
+    with open(f"{OUTDIR}/{file}_{TIMESTAMP}.json", "w") as f:
         f.writelines(r.text)
         f.close()
