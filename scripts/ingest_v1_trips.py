b6d681b95ad83a6a66b202b09bb6fe78503131fa
Fabian Thorsen <thorsen.fabian@gmail.com>
2023-01-22 21:39:11 +0100

    minor change in v1 file

--- scripts/ingest_v1_trips.py
+++ scripts/ingest_v1_trips.py
@@ -6,15 +6,18 @@ import requests
 BASE_URL = "https://data.urbansharing.com"
 ENDPOINT = "oslobysykkel.no/trips/v1"
 OUTDIR = "./data/trips/v1"
+YEARS = [2019, 2020, 2021, 2022, 2023]
+MONTHS = range(1, 13)
 
 if not os.path.exists(OUTDIR):
     os.makedirs(OUTDIR)
 
-for year in range(2019, 2024):
+# TODO: Rewrite with urllib
+for year in YEARS:
     if not os.path.exists(f"{OUTDIR}/{year}/"):
         os.mkdir(f"{OUTDIR}/{year}/")
 
-        for month in range(1, 13):
+        for month in MONTHS:
             r = requests.get(f"{BASE_URL}/{ENDPOINT}/{year}/{month:02d}.json")
             if r.status_code == 200:
                 with open(f"{OUTDIR}/{year}/{month:02d}.json", "w") as f:
